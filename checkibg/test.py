c0f1eada0690e36b1fd7767df5029190f39ad42e
adizalmanovich <adizalmanovich@gmail.com>
2020-10-10 12:42:41 +0000

    this is my testing

---
+++ checkibg/test.py
@@ -0,0 +1,24 @@
+import requests
+import sys
+
+api_url = sys.argv[1]
+
+def test_get_workers():
+    workers = requests.get(url=f"{api_url}/teams")
+    if workers.status_code != 200:
+        return False
+    return True
+
+def test_add_workers():
+    workers = requests.post(url=f"{api_url}/teams")
+    if workers.status_code != 200:
+        return False
+    return True
+
+
+
+
+
+
+print(test_get_workers())
+
