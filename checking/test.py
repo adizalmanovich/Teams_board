fdd064197a4dff9743161771c3db16b743b84a2d
adizalmanovich <adizalmanovich@gmail.com>
2020-10-11 07:57:39 +0000

    this is my changres

--- checking/test.py
+++ checking/test.py
@@ -4,13 +4,13 @@ import sys
 api_url = sys.argv[1]
 
 def test_get_workers():
-    workers = requests.get(url=f"{api_url}/teams")
+    workers = requests.get(url=f"{api_url}/api/teams")
     if workers.status_code != 200:
         return False
     return True
 
 def test_add_workers():
-    workers = requests.post(url=f"{api_url}/teams")
+    workers = requests.post(url=f"{api_url}/api/teams")
     if workers.status_code != 200:
         return False
     return True
